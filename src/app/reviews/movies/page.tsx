216206dfc1e78c95fe09f0dbcfefaaebecf07b7f
Görkem Eldeniz <82877261+GorkemEldeniz@users.noreply.github.com>
2024-09-23 18:49:04 +0300

    [add] boilerplate page

---
+++ src/app/reviews/movies/page.tsx
@@ -0,0 +1,5 @@
+// TODO: Add movie reviews page
+
+export default function MoviesPage() {
+	return <div>Movies</div>;
+}
---
+++ src/app/reviews/tv-series/page.tsx
@@ -0,0 +1,5 @@
+// TODO: Add TV series reviews page
+
+export default function TVSeriesPage() {
+	return <div>TV Series</div>;
+}
